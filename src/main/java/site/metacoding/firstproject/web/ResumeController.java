37707b7471bf4076de9f12beb39f9317d59d6794
kongjarvan <wjsdudwox@gmail.com>
2022-10-06 14:45:29 +0900

    jsp를 통한 resume insert 테스트완료 (boolean 입력값 받기 공부 필요)

--- src/main/java/site/metacoding/firstproject/web/ResumeController.java
+++ src/main/java/site/metacoding/firstproject/web/ResumeController.java
@@ -1,6 +1,7 @@
 package site.metacoding.firstproject.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -9,23 +10,26 @@ import lombok.RequiredArgsConstructor;
 import site.metacoding.firstproject.domain.resume.Resume;
 import site.metacoding.firstproject.service.ResumeService;
 
-
 @RequiredArgsConstructor
 @Controller
 public class ResumeController {
-    
+
     private final ResumeService resumeService;
 
+    @GetMapping("/resume/insert")
+    public String resumeInsertForm() {
+        return "resume/insert";
+    }
 
-    @PostMapping("/resume/post")
-    public String insertResume(Resume resume){
+    @PostMapping("/resume/insert")
+    public String insertResume(Resume resume) {
         resumeService.이력서작성(resume);
-        return"/";
+        return "redirect:/";
     }
 
     @PutMapping("/resume/{resumeId}/update")
-    public String updateResume(@PathVariable Integer resumeId, Resume resume){
+    public String updateResume(@PathVariable Integer resumeId, Resume resume) {
         resumeService.이력서수정(resumeId, resume);
-        return"/";
+        return "/";
     }
 }
