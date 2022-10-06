f3874698d36c162d04eda51be1b6071781ae6bcc
mb6282 <marbea6282@gmail.com>
2022-10-06 10:52:14 +0900

    notice 엔티티 및 다오설계

---
+++ src/main/java/site/metacoding/firstproject/domain/notice/NoticeDao.java
@@ -0,0 +1,17 @@
+package site.metacoding.firstproject.domain.notice;
+
+import java.util.List;
+
+public interface NoticeDao {
+    public List<Notice> findAll();
+
+    public List<Notice> findByJobNotice();
+
+    public Notice findById(Integer id);
+
+    public void insert(Notice notice);
+
+    public void update(Integer id, Notice notice);
+
+    public void deleteById(Integer id);
+}
