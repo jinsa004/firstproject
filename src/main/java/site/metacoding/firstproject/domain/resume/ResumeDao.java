b25aed4a442d03812b492eb4825228e99920a583
hyonna12 <hyonna01@gmail.com>
2022-10-11 14:49:14 +0900

    companyUpdate mapper 작성

--- src/main/java/site/metacoding/firstproject/domain/resume/ResumeDao.java
+++ src/main/java/site/metacoding/firstproject/domain/resume/ResumeDao.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 public interface ResumeDao {
     public List<Resume> findAll();
+
     public Resume findById(Integer resumeId);
+
     public void insert(Resume resume);
+
     public void update(Integer resumeId, Resume resume);
+
     public void deleteById(Integer resumeId);
 }
